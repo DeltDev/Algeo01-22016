4021d58b13fe41759a00189a135cf9a458730b59
Devinzenzhang <devinzenzhang@gmail.com>
2023-09-20 19:48:04 +0700

    Membuat menu (setelah fungsi jadi tambahin)

--- Algeo0122016/src/Main.java
+++ Algeo0122016/src/Main.java
@@ -7,20 +7,71 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		int row1,col1;
-		row1 = in.nextInt();
-		col1 = in.nextInt();
-		Matrix mat1 = new Matrix(row1,col1);
-		mat1.inputMatrix();
-		mat1.printMatrix();
-		int row2,col2;
-		row2 = in.nextInt();
-		col2 = in.nextInt();
-		Matrix mat2 =new Matrix(row2,col2);
-		mat2.inputMatrix();
-		mat2.printMatrix();
-		Matrix mat3 = new Matrix(row1,col2);
-		mat3 = Matrix.Multiply(mat1,mat2);
-		mat3.printMatrix();
+		int inputMenu, inputSubmenu;
+		while (true){
+			System.out.println("MENU");
+			System.out.println("1. Sistem Persamaan Linier");
+			System.out.println("2. Determinan");
+			System.out.println("3. Matriks balikan");
+			System.out.println("4. Interpolasi Polinom");
+			System.out.println("5. Interpolasi Bicubic Spline");
+			System.out.println("6. Regresi linier berganda");
+			System.out.println("7. Keluar");
+			inputMenu = in.nextInt();
+			switch (inputMenu){
+				case 1:
+					while (true){
+						System.out.println("SISTEM PERSAMAAN LINIER");
+						System.out.println("1. Metode eliminasi Gauss");
+						System.out.println("2. Metode eliminasi Gauss-Jordan");
+						System.out.println("3. Metode matriks balikan");
+						System.out.println("4. Kaidah Cramer");
+						inputSubmenu = in.nextInt();
+						switch (inputSubmenu){
+							case 1:
+								// fungsi SPL Gauss
+								break;
+							case 2:
+								// fungsi SPL Gauss-Jordan
+								break;
+							case 3:
+								// fungsi SPL matriks balikan
+								break;
+							case 4:
+								// fungsi SPL Cramer
+								break;
+							default:
+								System.out.println("Masukkan angka diantara 1 sampai 4 -_-");
+						};
+						if ((inputSubmenu > 0) && (inputSubmenu < 5)){
+							break;
+						};
+					};
+					break;
+				case 2:
+					System.out.println("DETERMINAN"); // nanti
+					break;
+				case 3:
+					System.out.println("MATRIKS BALIKAN"); // nanti
+					break;
+				case 4:
+					System.out.println("INTERPOLASI POLINOM"); // nanti
+					break;
+				case 5:
+					System.out.println("INTERPOLASI BICUBIC SPLINE"); // nanti
+					break;
+				case 6:
+					System.out.println("REGRESI LINIER BERGANDA"); // nanti
+					break;
+				case 7:
+					System.out.println("KELUAR"); // nanti
+					break;
+				default:
+					System.out.println("Masukkan angka diantara 1 sampai 7 -_-");
+			};
+			if (inputMenu == 7){ // keluar
+				break;
+			};
+		};
 	}
 }
