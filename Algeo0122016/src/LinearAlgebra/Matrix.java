b79a788fa0bb0842d32d7048b93903451cb862d2
DeltDev <deltenc88@gmail.com>
2023-09-23 17:03:10 +0700

    RAISE MODULARITY!!! and removed unnecessary commands

--- Algeo0122016/src/LinearAlgebra/Matrix.java
+++ Algeo0122016/src/LinearAlgebra/Matrix.java
@@ -20,17 +20,6 @@ public class Matrix {
 		}
 	}
 	
-	//prosedur untuk input elemen matrix di baris row dan kolom col
-	public void inputELMT(double val, int row, int col) {
-		this.Mat[row][col] = val;
-	}
-	
-	//Selektor untuk mengakses Elemen di baris row dan col
-	public double ELMT(int row, int col){
-		
-		return this.Mat[row][col];
-	}
-	
 	//Prosedur untuk input matriks
 	public void inputMatrix() {
 		Scanner in = new Scanner(System.in);
@@ -83,34 +72,24 @@ public class Matrix {
 		return ret;
 	}
 	//prosedur untuk menambahkan matriks
-	public static Matrix Plus(Matrix m1, Matrix m2)
+	public static Matrix PlusMinus(Matrix m1, Matrix m2,boolean minus)
 	{
-		int i,j;
+		int i,j,sign;
+		sign = 1;
+		if(minus) {
+			sign *= -1;
+		}
 		Matrix ret;
 		ret = new Matrix(m1.row,m1.col);
 		for(i=0;i<m1.row;i++) {
 			for(j=0;j<m1.col;j++)
 			{
-				ret.Mat[i][j] = m1.Mat[i][j] + m2.Mat[i][j]; 
+				ret.Mat[i][j] = m1.Mat[i][j] + m2.Mat[i][j] * sign; 
 			}
 		}
 		return ret;
 	}
 	
-	//prosedur untuk mengurangi matriks
-	public static Matrix Minus(Matrix m1, Matrix m2)
-	{
-		int i,j;
-		Matrix ret;
-		ret = new Matrix(m1.row,m1.col);
-		for(i=0;i<m1.row;i++) {
-			for(j=0;j<m1.col;j++)
-			{
-				ret.Mat[i][j] = m1.Mat[i][j] - m2.Mat[i][j]; 
-			}
-		}
-		return ret;
-	}
 	
 	//prosedur perkalian matriks
 	public static Matrix Multiply(Matrix m1, Matrix m2) {
@@ -127,98 +106,4 @@ public class Matrix {
 		}
 		return ret;
 	}
-
-	//fungsi mengubah matriks menjadi matriks segitiga ATAS
-	public static double DeterminanReduksiBaris(Matrix m){
-		int n = m.row;
-		Matrix m1 = new Matrix(n,n);
-		double ratio,temp;
-		int swapcount;
-		swapcount = 0;
-		for(int i = 0; i<n; i++) {//copy matrix m ke m1 lokal
-			for(int j = 0; j<n; j++) {
-				m1.Mat[i][j] = m.Mat[i][j];
-			}
-		}
-		for(int i = 0; i<n-1; i++) { //ubah matrix m jadi matrix segitiga Atas
-			for(int j = n-1; j>i; j--) {
-				if(m1.Mat[j][i] == 0) {
-					continue;
-				} else {
-					if(m1.Mat[j-1][i] == 0) {
-						m1.swapRow(j,j-1);
-						swapcount++;
-						continue;
-					}
-						
-					ratio = m1.Mat[j][i] / m1.Mat[j-1][i];
-					for(int l = 0; l<n; l++) {
-						m1.Mat[j][l] = m1.Mat[j][l] - ratio * m1.Mat[j-1][l];
-					}
-				}
-			}
-		}
-		
-		double det;
-		det = 1;
-		for(int i = 0; i<n; i++) {
-			det *= m1.Mat[i][i];
-		}
-		
-		if(swapcount % 2 == 0) {
-			return det;
-		} else {
-			return det * (-1);
-		}
-	}
-
-/*	//fungsi menghitung determinan dengan ekspansi kofaktor
-	public static double DeterminanKofaktor(Matrix m){
-		if (m.row == 1){return m.Mat[0][0];} else { // kasus matriks ukuran 1x1
-			double Sum = 0;
-			Matrix ReCursed;
-			ReCursed = new Matrix((m.row - 1), (m.col - 1)); //matriks yang akan digunakan untuk rekursi jadi row dan col nya dikurangi 1
-			int i, j;
-			for (i = 1;i < m.row;i++){
-				for (j = 1;j < m.col;j++){
-					ReCursed.Mat[i-1][j-1] = m.Mat[i][j]; // copy tapi tanpa baris dan kolom pertama
-				};
-			};
-			Sum += (DeterminanKofaktor(ReCursed) * m.Mat[0][0]); // first
-			for (i = 1;i < m.row;i++){
-				for (j = 1;j < m.col;j++){
-					ReCursed.Mat[i-1][j-1] = m.Mat[i-1][j]; // untuk yang selanjutnya copy 1 baris
-				};
-				if (i % 2 == 0){Sum += (DeterminanKofaktor(ReCursed) * m.Mat[i][0]);} else {Sum -= (DeterminanKofaktor(ReCursed) * m.Mat[i][0]);}
-			};
-			return Sum;
-		}
-	}
-*/
-	/*public static void CramerSolve(Matrix m) {
-		double D;
-		D = Matrix.DeterminanKofaktor(m);
-		
-		if(D == 0) {
-			System.out.println("Sistem Persamaan Linear berikut tidak memiliki solusi.");
-			return;
-		}
-		Matrix clone,reset;
-		clone = new Matrix(m.row,m.col);
-		clone = m;
-		reset = new Matrix(m.row,m.col);
-		reset = m;
-		double [] solution = new double[m.row];
-		clone.printMatrix();
-		for(int i = 0; i<m.col-1; i++) {
-			for(int j = 0; j<m.row; j++) {
-				clone.Mat[j][i] = m.Mat[j][m.col-1];
-			}
-			clone.printMatrix();
-			System.out.println("");
-			for(int j = 0; j<m.row; j++) {
-				clone.Mat[j][i] = reset.Mat[j][i];
-			}
-		}
-	}*/
 }
